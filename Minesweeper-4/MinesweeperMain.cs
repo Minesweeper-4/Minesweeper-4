97a83f2bf97bd4a87c1584d7032996238652bda1
ni4ka7a <n.atanasow@yahoo.com>
2015-09-14 15:19:51 -0700

    Added class ScoreRecord in another file.

--- Minesweeper-4/MinesweeperMain.cs
+++ Minesweeper-4/MinesweeperMain.cs
@@ -1,42 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-// testvano e - ba4ka, ne pipaj!!!!!!!
-
-namespace mini4ki
+﻿namespace Minesweeper
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class MinesweeperMain
     {
-        public class ScoreRecord
-        {
-            string personName;
-            int scorePoints;
-
-            public string PersonName
-            {
-                get { return personName; }
-                set { personName = value; }
-            }
-            public int ScorePoints
-            {
-                get { return scorePoints; 
-				
-				
-				}
-                set { scorePoints = value; }
-            }
-
-            public ScoreRecord() { }
-
-            public ScoreRecord(string personName, int points)
-            {
-                this.personName = personName;
-                this.scorePoints = points;
-            }
-
-        }
 
         static void Main(string[] args)
         {
---
+++ Minesweeper-4/ScoreRecord.cs
@@ -0,0 +1,34 @@
+﻿namespace Minesweeper
+{
+    using System;
+
+    public class ScoreRecord
+    {
+        string personName;
+        int scorePoints;
+
+        public string PersonName
+        {
+            get { return personName; }
+            set { personName = value; }
+        }
+        public int ScorePoints
+        {
+            get
+            {
+                return scorePoints;
+
+
+            }
+            set { scorePoints = value; }
+        }
+
+        public ScoreRecord() { }
+
+        public ScoreRecord(string personName, int points)
+        {
+            this.personName = personName;
+            this.scorePoints = points;
+        }
+    }
+}
