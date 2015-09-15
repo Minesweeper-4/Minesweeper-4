f73f16f19622181735c58773086c5e08f189d31e
VladimirDimov <vdimov1124@gmail.com>
2015-09-15 15:11:07 +0300

    Printer added class

--- Minesweeper-4/Engine/MinesweeperEngine.cs
+++ Minesweeper-4/Engine/MinesweeperEngine.cs
@@ -35,21 +35,21 @@
                     matrixLogic.InitBoard(matrix);
 
                     int boardRows = matrix.GetLength(0);
-            int boardColumns = matrix.GetLength(1);
-            Console.WriteLine("\n    0 1 2 3 4 5 6 7 8 9");
-            Console.WriteLine("   ---------------------");
+                    int boardColumns = matrix.GetLength(1);
+                    Console.WriteLine("\n    0 1 2 3 4 5 6 7 8 9");
+                    Console.WriteLine("   ---------------------");
 
-            for (int i = 0; i < boardRows; i++)
-            {
-                Console.Write("{0} | ", i);
-                for (int j = 0; j < boardColumns; j++)
-                {
-                    Console.Write(string.Format("{0} ",matrix[i, j].CurrentSymbol));
-                }
-                Console.Write("|");
-                Console.WriteLine();
-            }
-            Console.WriteLine("   ---------------------\n");
+                    for (int i = 0; i < boardRows; i++)
+                    {
+                        Console.Write("{0} | ", i);
+                        for (int j = 0; j < boardColumns; j++)
+                        {
+                            Console.Write(string.Format("{0} ", matrix[i, j].CurrentSymbol));
+                        }
+                        Console.Write("|");
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("   ---------------------\n");
 
                     break;
                 case "restart":
@@ -103,6 +103,26 @@
 
 
                     //}
+                    break;
+                case "mode":
+                    if (command.Parameters.Count != 1)
+                    {
+                        Console.WriteLine("Invalid mode!");
+                    }
+
+                    if (command.Parameters[0] == "standard")
+                    {
+                        // TODO
+                    }
+                    else if (command.Parameters[0] == "fancy")
+                    {
+                        // TODO
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid mode!");
+                    }
+
                     break;
                 default:
                     Console.WriteLine("\nIllegal move!\n");
---
+++ Minesweeper-4/Logic/Draw/IPrinter.cs
@@ -0,0 +1,14 @@
+﻿using Minesweeper.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Logic.Draw
+{
+    interface IPrinter
+    {
+        void Print(IMatrix matrix);
+    }
+}
