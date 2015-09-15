6a72233d8b460df05e62a7d60cd8bae259d10664
VladimirDimov <vdimov1124@gmail.com>
2015-09-15 10:55:38 +0300

    Added class Player

---
+++ Minesweeper-4/Data/Player.cs
@@ -0,0 +1,32 @@
+﻿namespace Minesweeper.Data
+{
+    using Minesweeper.Interfaces;
+
+    class Player : IPlayer
+    {
+
+        public string NickName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public int Scores
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Minesweeper-4/Interfaces/IPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Interfaces
+{
+    interface IPlayer
+    {
+        public string NickName { get; set; }
+
+        public int Scores { get; set; }        
+    }
+}
