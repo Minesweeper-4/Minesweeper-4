25c8c94d45694c404201ea7769edf64a518e9ed2
MarinMarinov <marin.telerik@gmail.com>
2015-09-28 15:03:18 +0300

    Matrix Class tests for SaveMememento and RestoreMemento methods. Total Code Coverage - 38%

---
+++ NunitTests/MatrixClassTests.cs
@@ -0,0 +1,31 @@
+﻿using Minesweeper.Data;
+using Minesweeper.Interfaces;
+using NUnit.Framework;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class MatrixClassTests
+    {
+        [Test]
+        public void MatrixSaveMementoAndRestoreMementoMethodsWorksProper()
+        {
+            ICell[,] fieldExpected = new ICell[3, 7];
+
+            Matrix matrix = new Matrix();
+
+            matrix.Field = fieldExpected;
+
+            MatrixMemento matrixMemento = matrix.SaveMemento();
+
+            matrix.Field = new ICell[12, 18];
+
+            matrix.RestoreMemento(matrixMemento); // Comment this row and the test will fail 
+
+            Assert.AreEqual(fieldExpected.GetLength(0), matrix.Rows,
+                "Matrix SaveMememento and RestoreMememento doesn't work proper"); 
+            Assert.AreEqual(fieldExpected.GetLength(1), matrix.Cols,
+                "Matrix SaveMememento and RestoreMememento doesn't work proper");
+        }
+    }
+}
