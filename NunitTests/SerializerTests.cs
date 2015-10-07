380c9b39333b6eaff4c03cda1210e03f81801549
ni4ka7a <n.atanasow@yahoo.com>
2015-10-07 14:43:55 +0300

    Serializer tests added.

---
+++ NunitTests/SerializerTests.cs
@@ -0,0 +1,40 @@
+﻿using Minesweeper.Helpers;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class SerializerTests
+    {
+        [Test]
+        public void ExpectSerializeMethodToCreateFileIfTheFileDoesntExist()
+        {
+            var fileName = "testFile.txt";
+            var testObjectToSerialize = new Object();
+            var serializer = new Serializer();
+
+            serializer.Serialize(testObjectToSerialize, fileName);
+
+            Assert.True(File.Exists(fileName));
+            File.Delete(fileName);
+        }
+
+        [Test]
+        public void ExpectDeserializeMethodToReturnObjectWhenFileExist()
+        {
+            var fileName = "testFile.txt";
+            var testObjectToSerialize = new Object();
+            var serializer = new Serializer();
+
+            serializer.Serialize(testObjectToSerialize, fileName);
+
+            var deserializedObject = serializer.Deserialize(fileName);
+
+            Assert.NotNull(deserializedObject);
+
+            File.Delete(fileName);
+        }
+    }
+}
